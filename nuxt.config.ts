1a17723905e8c879224fc9e0d19df56e82a670d7
huyentran <huyentran@ww-system.com>
2025-04-02 12:57:27 +0700

    modify2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
-
-// // https://nuxt.com/docs/api/configuration/nuxt-config
 // export default defineNuxtConfig({
-//   ssr: false,
-//   runtimeConfig: {
-//       // Public keys that are exposed to the client
-//       public: {
-//           apiBase: 'https://ohk-news.g.kuroco-front.app'
-//       }
-//   },
+//   compatibilityDate: '2024-11-01',
+//   devtools: { enabled: true }
 // })
+
+
+export default defineNuxtConfig({
+  ssr: false,
+  runtimeConfig: {
+      // Public keys that are exposed to the client
+      public: {
+          apiBase: 'https://ohk-news.g.kuroco-front.app'
+      }
+  },
+})
